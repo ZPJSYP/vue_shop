804794e4efa315a6af312f9112bdf330d6d610ea
Pengjian Zhang <894293752@qq.com>
2020-04-05 17:24:37 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Home组件</h1>
+    <el-button type="primary" @click="logout">退出登录</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// $message为自定义属性，把弹框组件挂载到vue的原型对象上，所有组建都可以通过this.$message来访问弹框组件。
+Vue.prototype.$message = Message
